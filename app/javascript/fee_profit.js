5c00a6da7ad7107cdec1f9f87ccf463179037928
chiakirinn1717 <chiakirinn1717@gmail.com>
2025-05-17 21:47:36 +0900

    レビュー指摘修正

--- app/javascript/fee_profit.js
+++ app/javascript/fee_profit.js
@@ -1,7 +1,7 @@
 function calc (){
     const price = document.getElementById("item-price")
     price.addEventListener("input", () => {
-        let tax = Number(price.value) * 0.1
+        let tax = Math.floor(Number(price.value) * 0.1)
         let fixedTax = Math.floor(tax)
         document.getElementById("add-tax-price").innerHTML = fixedTax
         document.getElementById("profit").innerHTML = Number(price.value) - fixedTax
