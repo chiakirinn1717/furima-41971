d8b08a10c4791f5eb0213a9521da03745958612b
chiakirinn1717 <chiakirinn1717@gmail.com>
2025-05-18 23:51:10 +0900

    first commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :delivery_day
   belongs_to :user
+  has_one :history
 
   validates :image, presence: true
   validates :title, presence: true
